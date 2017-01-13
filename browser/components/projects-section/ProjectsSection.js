7cae03753d0e97d47942eafd86fdf6b597197f33
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-01-13 15:52:02 -0500

    added the 4th video

--- browser/components/projects-section/ProjectsSection.js
+++ browser/components/projects-section/ProjectsSection.js
@@ -10,10 +10,10 @@ export default class ProjectsSection extends Component {
       <div className="projects">
         <Row>
         <h1 id="projects-title">PROJECTS / TALKS</h1>
-          <Col sm={12} md={4} lg={4}>
+          <Col sm={12} md={6} lg={3}>
             <div className="statement">
               <h1>Loop</h1>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
+              <iframe width="500" height="281" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
               <p>
                 <span>
                 Completely re-imagines the student to teacher interaction model in the classroom
@@ -21,10 +21,10 @@ export default class ProjectsSection extends Component {
               </p>
             </div>
           </Col>
-          <Col sm={12} md={4} lg={4}>
+          <Col sm={12} md={6} lg={3}>
             <div className="statement">
               <h1>Tails</h1>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
+              <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
               <p>
                 <span>
                 Hey, I'm George. I enjoy creating gorgeous applications,
@@ -33,10 +33,22 @@ export default class ProjectsSection extends Component {
               </p>
             </div>
           </Col>
-          <Col sm={12} md={4} lg={4}>
+          <Col sm={12} md={6} lg={3}>
             <div className="statement">
               <h1>Cube Master</h1>
-              <iframe width="560" height="315" src="https://www.youtube.com/embed/PNkrEoNgo-A" frameBorder="0" allowFullScreen></iframe>
+              <iframe width="500" height="281" src="https://www.youtube.com/embed/PNkrEoNgo-A" frameBorder="0" allowFullScreen></iframe>
+              <p>
+                <span>
+                Hey, I'm George. I enjoy creating gorgeous applications,
+                and making objects in virtual reality.
+                </span>
+              </p>
+            </div>
+          </Col>
+          <Col sm={12} md={6} lg={3}>
+            <div className="statement">
+              <h1>Home Cooked</h1>
+              <iframe width="500" height="281" src="https://www.youtube.com/embed/aQAHGaJZjM8?list=PLx0iOsdUOUmk4F-mEQuJsLn41qXF5alF9" frameBorder="0" allowFullScreen></iframe>
               <p>
                 <span>
                 Hey, I'm George. I enjoy creating gorgeous applications,
