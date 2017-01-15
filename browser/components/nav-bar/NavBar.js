63b06949ff4b40770049911e26017a2398d2b688
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-01-14 23:16:52 -0500

    changed projects rendering

--- browser/components/nav-bar/NavBar.js
+++ browser/components/nav-bar/NavBar.js
@@ -12,24 +12,10 @@ export default class NavigationBar extends Component {
       <div className="navbar-fixed navigation">
         <Navbar brand='logo' right>
           <NavItem href='#AB-me'>Getting started</NavItem>
-          <NavItem href='components.html'>Components</NavItem>
+          <NavItem href="#projects">Projects</NavItem>
+          <NavItem href='#contact'>Contact Me</NavItem>
         </Navbar>
       </div>
     );
   }
 }
-        // <Navbar fixedTop collapseOnSelect>
-        //   <Navbar.Header>
-        //     <Navbar.Brand>
-        //       <a href="#">GSS</a>
-        //     </Navbar.Brand>
-        //     <Navbar.Toggle />
-        //   </Navbar.Header>
-        //   <Navbar.Collapse>
-        //     <Nav pullRight>
-        //       <NavItem eventKey={1} href="#projects">Projects</NavItem>
-        //       <NavItem eventKey={2} href="#AB-me">About</NavItem>
-        //       <NavItem href="#contact">Contact</NavItem>
-        //     </Nav>
-        //   </Navbar.Collapse>
-        // </Navbar>
--- browser/components/projects-section/ProjectsSection.js
+++ browser/components/projects-section/ProjectsSection.js
@@ -11,10 +11,12 @@ export default class ProjectsSection extends Component {
           <a name="projects"></a>
             <Row>
               <h1 id="projects-title">PROJECTS / TALKS</h1>
-                <Col xs={12} sm={12} md={6} lg={6}>
+                <Col xs={12} sm={12} md={6} lg={3}>
                   <div className="statement">
                     <h2>Loop</h2>
-                    <iframe width="500" height="281" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
+                    <div className="video-container">
+                      <iframe width="500" height="281" src="https://www.youtube.com/embed/3pzsP2I7e3U" frameBorder="0" allowFullScreen></iframe>
+                    </div>
                     <p>
                       <span>
                       Completely re-imagines the student to teacher interaction model in the classroom
@@ -22,10 +24,12 @@ export default class ProjectsSection extends Component {
                     </p>
                   </div>
                 </Col>
-                 <Col xs={12} sm={12} md={4} lg={6}>
+                 <Col xs={12} sm={12} md={6} lg={3}>
                   <div className="statement">
                     <h2>Tails</h2>
-                    <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
+                    <div className="video-container">
+                      <iframe width="500" height="281" src="https://www.youtube.com/embed/A-LKDSz4Ik4" frameBorder="0" allowFullScreen></iframe>
+                    </div>
                     <p>
                       <span>
                       A brief talk about remaining anonymous online using TAIL, and the TOR network.
@@ -34,10 +38,12 @@ export default class ProjectsSection extends Component {
                   </div>
                 </Col>
 
-                <Col xs={12} sm={12} md={6} lg={6}>
+                <Col xs={12} sm={12} md={6} lg={3}>
                   <div className="statement">
                     <h2>Cube Master</h2>
-                    <iframe width="500" height="281" src="https://www.youtube.com/embed/PNkrEoNgo-A" frameBorder="0" allowFullScreen></iframe>
+                    <div className="video-container">
+                      <iframe width="500" height="281" src="https://www.youtube.com/embed/PNkrEoNgo-A" frameBorder="0" allowFullScreen></iframe>
+                    </div>
                     <p>
                       <span>
                       Unitys "Roll A Ball" tutorial remade in Virtual Reality.
@@ -45,10 +51,12 @@ export default class ProjectsSection extends Component {
                     </p>
                   </div>
                 </Col>
-                <Col xs={12} sm={12} md={6} lg={6}>
+                <Col xs={12} sm={12} md={6} lg={3}>
                   <div className="statement">
                     <h2>Home Cooked</h2>
-                    <iframe width="500" height="281" src="https://www.youtube.com/embed/aQAHGaJZjM8?list=PLx0iOsdUOUmk4F-mEQuJsLn41qXF5alF9" frameBorder="0" allowFullScreen></iframe>
+                    <div className="video-container">
+                      <iframe width="500" height="281" src="https://www.youtube.com/embed/aQAHGaJZjM8?list=PLx0iOsdUOUmk4F-mEQuJsLn41qXF5alF9" frameBorder="0" allowFullScreen></iframe>
+                    </div>
                     <p>
                       <span>
                       A basic receipe site, using Yummlys Api to match search querys with ingredients.
