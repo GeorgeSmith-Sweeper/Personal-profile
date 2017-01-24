6348c2f7b1d91cd0540c3c8c6fe44564a7f41065
George Smith-Sweeper <george.smsweeper@gmail.com>
2017-01-24 13:27:55 -0500

    added a file path

--- server.js
+++ server.js
@@ -3,11 +3,12 @@
 const express = require('express');
 const volleyball = require('volleyball');
 
+let path = require('path')
 const app = express();
 
 app.use(volleyball);
-
 app.use(express.static(__dirname));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'public/index.html')));
 
 const server = app.listen(process.env.PORT || 3000, function () {
   console.log((`Listening on ${JSON.stringify(server.address())}`))
